ad59a8c5ffdf89083fbe9c599705a50a946a1d23
Anatoly <nachan2017@yandex.ru>
2022-12-04 16:29:59 +0300

    Задача 2_2

---
+++ Task2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int n = new Random().Next(100, 1000);
+Console.WriteLine(n);
+
+int numberA = n / 100;
+int numberB = n % 10;
+int numberC = numberA * 10 + numberB;
+
+Console.WriteLine($"Полученное число: {numberC}");
